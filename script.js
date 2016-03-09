22c54d2a497f69bd9d0462a9586f4155567e47e5
Alex Bullard <abullrd@gmail.com>
2016-03-08 21:25:23 -0800

    commit

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    $('.li_food').hover(
+        function(){
+                $('.food').fadeIn();
+            },
+        function(){
+                $('.food').fadeOut();
+            }
+            );
+    
+    
+    $('.li_attractions').hover(
+        function(){
+                $('.attractions').fadeIn();
+            },
+        function(){
+                $('.attractions').fadeOut();
+            }
+            )
+    
+    
+    $('.li_outdoors1').hover(
+        function(){
+                $('.outdoors1').fadeIn();
+            },
+        function(){
+                $('.outdoors1').fadeOut();
+            }
+            )
+    
+    $('.li_nightlife1').hover(
+        function(){
+                $('.nightlife1').fadeIn();
+            },
+        function(){
+                $('.nightlife1').fadeOut();
+            }
+            )
+    
+
+}
+);
